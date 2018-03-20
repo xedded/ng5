32194c25ec5dee27a2e25fef46ce7f4400d5d266
pontus.segerstad@gmail.com <pontus.segerstad@gmail.com>
2018-03-20 22:04:04 +0100

    Okey

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,16 +12,16 @@ import { DataService } from '../data.service';
         query(':enter', style({ opacity: 0 }), { optional: true }),
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({ opacity: 0.5, transform: 'translateY(35px)', offset: .3}),
-            style({ opacity: 1, transform: 'translateY(0)', offset: 1}),
+            style({ opacity: 0, transform: 'translateX(75%)', offset: 0}),
+            style({ opacity: 0.5, transform: 'translateX(-35px)', offset: .3}),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 1}),
           ]))]), { optional: true }),
 
         query(':leave', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({ opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({ opacity: 0.5, transform: 'translateY(35px)', offset: .3}),
-            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 0}),
+            style({ opacity: 0.5, transform: 'translateX(-35px)', offset: .3}),
+            style({ opacity: 0, transform: 'translateX(25%)', offset: 1}),
           ]))]), { optional: true })
       ])
     ])
